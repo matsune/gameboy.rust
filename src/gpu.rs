c846fa7808c92665b8278a54bd9bbec535027c33
Yuma Matsune <yuma.matsune@gmail.com>
2019-05-31 19:00:41 +0900

    implement MBC3

--- src/gpu.rs
+++ src/gpu.rs
@@ -21,12 +21,6 @@ impl std::convert::From<u8> for Lcdc {
     }
 }
 
-impl Default for Lcdc {
-    fn default() -> Self {
-        Self { inner: 0b0100_1000 }
-    }
-}
-
 impl Lcdc {
     fn get(&self) -> u8 {
         self.inner
@@ -132,20 +126,25 @@ pub struct GPU {
     bg_prio: [BgPrio; SCREEN_W],
 }
 
-impl Default for GPU {
-    fn default() -> Self {
+impl GPU {
+    pub fn new(skip_boot: bool) -> Self {
+        let (lcdc, bgp, obp0, obp1) = if skip_boot {
+            (Lcdc::from(0x91), 0xfc, 0xff, 0xff)
+        } else {
+            (Lcdc::from(0x48), 0x00, 0x00, 0x01)
+        };
         Self {
             blanked: false,
             data: [[[0xffu8; 3]; SCREEN_W]; SCREEN_H],
             redraw: false,
-            bgp: 0x00,
+            bgp,
             clocks: 0,
-            lcdc: Lcdc::default(),
+            lcdc,
             ly: 0x00,
             ly_compare: 0x00,
             oam: RAM::new(0xfe00, 0xa0),
-            obp0: 0x00,
-            obp1: 0x01,
+            obp0,
+            obp1,
             vram: RAM::new(0x8000, 0x2000),
             scx: 0x00,
             scy: 0x00,
@@ -155,9 +154,6 @@ impl Default for GPU {
             bg_prio: [BgPrio::Normal; SCREEN_W],
         }
     }
-}
-
-impl GPU {
     pub fn get_rgb_data(&self) -> Vec<u8> {
         let mut res = Vec::new();
         for line in self.data.iter() {
@@ -389,71 +385,6 @@ impl GPU {
             }
         }
     }
-
-    //  fn draw_sprites(&mut self) {
-    //      if !self.lcdc.sprite_enabled() {
-    //          return;
-    //      }
-
-    //      for index in 0..40 {
-    //          let i = 39 - index;
-    //          let spriteaddr = 0xFE00 + (i as u16) * 4;
-    //          let spritey = self.read(spriteaddr + 0) as u16 as i32 - 16;
-    //          let spritex = self.read(spriteaddr + 1) as u16 as i32 - 8;
-    //          let tilenum = (self.read(spriteaddr + 2)
-    //              & (if self.lcdc.sprite_size() { 0xFE } else { 0xFF }))
-    //              as u16;
-    //          let flags = self.read(spriteaddr + 3) as usize;
-    //          let usepal1: bool = flags & (1 << 4) != 0;
-    //          let xflip: bool = flags & (1 << 5) != 0;
-    //          let yflip: bool = flags & (1 << 6) != 0;
-    //          let belowbg: bool = flags & (1 << 7) != 0;
-    //          let c_palnr = flags & 0x07;
-    //          let c_vram1: bool = flags & (1 << 3) != 0;
-
-    //          let line = self.ly as i32;
-    //          let sprite_size = if self.lcdc.sprite_size() { 16 } else { 8 };
-
-    //          if line < spritey || line >= spritey + sprite_size {
-    //              continue;
-    //          }
-    //          if spritex < -7 || spritex >= (SCREEN_W as i32) {
-    //              continue;
-    //          }
-
-    //          let tiley: u16 = if yflip {
-    //              (sprite_size - 1 - (line - spritey)) as u16
-    //          } else {
-    //              (line - spritey) as u16
-    //          };
-
-    //          let tileaddress = 0x8000u16 + tilenum * 16 + tiley * 2;
-    //          let (b1, b2) = (self.vram.read(tileaddress), self.vram.read(tileaddress + 1));
-
-    //          'xloop: for x in 0..8 {
-    //              if spritex + x < 0 || spritex + x >= (SCREEN_W as i32) {
-    //                  continue;
-    //              }
-
-    //              let xbit = 1 << (if xflip { x } else { 7 - x } as u32);
-    //              let colnr =
-    //                  (if b1 & xbit != 0 { 1 } else { 0 }) | (if b2 & xbit != 0 { 2 } else { 0 });
-    //              if colnr == 0 {
-    //                  continue;
-    //              }
-
-    //              if belowbg && self.bg_prio[(spritex + x) as usize] != BgPrio::Color0 {
-    //                  continue 'xloop;
-    //              }
-    //              let color = if usepal1 {
-    //                  Self::get_mono_color(self.obp1, colnr) as u8
-    //              } else {
-    //                  Self::get_mono_color(self.obp0, colnr) as u8
-    //              };
-    //              self.set_mono_color((spritex + x) as usize, color);
-    //          }
-    //      }
-    //  }
 }
 
 impl Memory for GPU {

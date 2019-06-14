11eb08a399dd45b435a49bfca11b49fbe0eb702a
Yuma Matsune <yuma.matsune@gmail.com>
2019-06-14 16:41:24 +0900

    implement Memory for Serial

--- src/serial.rs
+++ src/serial.rs
@@ -1,13 +1,25 @@
-// TODO:
-
 use crate::memory::Memory;
 
 #[derive(Default)]
-pub struct Serial {}
+pub struct Serial {
+    data: u8,
+    control: u8,
+}
 
 impl Memory for Serial {
-    fn read(&self, _address: u16) -> u8 {
-        0
+    fn read(&self, a: u16) -> u8 {
+        match a {
+            0xff01 => self.data,
+            0xff02 => self.control,
+            _ => panic!(),
+        }
     }
-    fn write(&mut self, _address: u16, _value: u8) {}
+
+    fn write(&mut self, a: u16, v: u8) {
+        match a {
+            0xff01 => self.data = v,
+            0xff02 => self.control = v,
+            _ => panic!(),
+        }
+    }
 }
